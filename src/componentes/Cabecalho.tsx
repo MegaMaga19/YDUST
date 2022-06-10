54b4c4d8a3fa58a4a9e6b80f9549ab5ee40d2a6e
DanigolXBr <DanigolXBr.2020@gmail.com>
2022-06-09 21:39:54 -0300

    Cria a interface do cabecalho

---
+++ src/componentes/Cabecalho.tsx
@@ -0,0 +1,15 @@
+import './Cabecalho.css'
+
+const Cabecalho = function () {
+    return (
+        <>
+            <div className='cabe'> .Ydust </div>
+            <div className='butao'>
+                <button className='cd'>Cadastro</button> 
+                <button className='entrar'>Entrar</button>
+            </div>
+        </>
+    );
+};
+
+export default Cabecalho;
